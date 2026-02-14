4098829012a7ec6c3c40d81c33e411abd5c7bc8c
Muhammad Zelvan <muhammadzelvan263@gmail.com>
2026-02-15 00:47:03 +0700

    fix: update image type declarations to include all lowercase extensions

--- types/images.d.ts
+++ types/images.d.ts
@@ -7,3 +7,28 @@ declare module "*.JPG" {
     const content: import("next/image").StaticImageData;
     export default content;
 }
+
+declare module "*.jpg" {
+    const content: import("next/image").StaticImageData;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    const content: import("next/image").StaticImageData;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: import("next/image").StaticImageData;
+    export default content;
+}
+
+declare module "*.gif" {
+    const content: import("next/image").StaticImageData;
+    export default content;
+}
+
+declare module "*.svg" {
+    const content: import("next/image").StaticImageData;
+    export default content;
+}
